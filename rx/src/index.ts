e4620876efb017377a735992e9e64e387b106936
Anton_Dmytriichuk <kaktusad@gmail.com>
2018-04-16 02:36:49 +0300

    Subject problems

--- rx/src/index.ts
+++ rx/src/index.ts
@@ -1,10 +1,25 @@
-import { of } from 'rxjs/observable/of';
-import { map } from 'rxjs/operators/map';
 import { Observable } from 'rxjs/Observable';
-import { takeEveryNth, fromLol } from './operators';
+import { takeOnly, toLoPerCase, symbolInterval, MiracleSubject } from './operators';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
-const source$: Observable<string> = fromLol();
-source$
-  .pipe(
-    map(value => value + '!!!')
-  ).subscribe(data => console.log(data));
+const source$: Observable<string> = symbolInterval(200);
+const vowels = 'AEIOUY';
+const sub = new MiracleSubject<number>(2);
+
+// source$
+//   .pipe(
+//     takeOnly(vowels),
+//     toLoPerCase
+//   ).subscribe(data => console.log(`Symbols: ${data}`));
+
+
+sub.subscribe(v => console.log('Sub1', v));
+sub.next(1);
+sub.next(2);
+sub.next(3); // Sub1 3
+sub.next(4); // Sub1 4
+sub.subscribe(v => console.log('Sub2', v));
+sub.next(5); // Sub1 5
+sub.next(6); // Sub1 6
+sub.next(7); // Sub1 7, Sub2 7
+sub.next(8); // Sub1 8, Sub2 8
--- rx/src/operators.ts
+++ rx/src/operators.ts
@@ -1,29 +1,112 @@
 import { Observable } from 'rxjs/Observable';
 import { Subscriber } from 'rxjs/Subscriber';
+import { async } from 'rxjs/scheduler/async';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
+import { ObjectUnsubscribedError } from 'rxjs/Rx';
+import { PartialObserver } from 'rxjs/Observer';
 
-export const takeEveryNth = (n: number) => <T>(source: Observable<T>) =>
-  new Observable<T>(observer => {
-    let count = 0;
+export const takeOnly = (s: string) => (source: Observable<String>) =>
+  new Observable<String>(observer => {
     return source.subscribe({
       next(x) {
-        if (count++ % n === 0) observer.next(x);
+        if (s.toUpperCase().includes(x.toUpperCase())) observer.next(x);
       },
-      error(err) { observer.error(err); },
-      complete() { observer.complete(); }
-    })
+      error(err) {
+        observer.error(err);
+      },
+      complete() {
+        observer.complete();
+      }
+    });
   });
 
-export function fromLol() {
-  return new Observable<string>(
-    (subscriber: Subscriber<string>) => {
-      setInterval(() => subscriber.next('S'), 1000);
-      subscriber.next('S');
-      if (!subscriber.closed) {
-        subscriber.complete();
+export const toLoPerCase = (source: Observable<String>) =>
+  new Observable<String>(observer => {
+    return source.subscribe({
+      next(x) {
+        const isUpper = x.charCodeAt(0) <= 90;
+        observer.next(isUpper ? x.toLowerCase() : x.toUpperCase());
+      },
+      error(err) {
+        observer.error(err);
+      },
+      complete() {
+        observer.complete();
       }
+    });
+  });
+
+export function symbolInterval(period: number, scheduler = async): Observable<string> {
+  if (!scheduler || typeof scheduler.schedule !== 'function') {
+    scheduler = async;
+  }
+
+  return new Observable<string>(subscriber => {
+    subscriber.add(
+      scheduler.schedule(dispatch, period, { subscriber, counter: getRandomSymbol(), period })
+    );
+    return subscriber;
+  });
+}
+
+interface SchedulerAction<T> extends Subscription {
+  schedule(state?: T, delay?: number): Subscription;
+}
+
+function dispatch(this: SchedulerAction<IntervalState>, state: IntervalState) {
+  const { subscriber, counter, period } = state;
+  subscriber.next(counter);
+  this.schedule({ subscriber, counter: getRandomSymbol(), period }, period);
+}
+
+function getRandomSymbol(): string {
+  const firstCharCode = 97;
+  const charsCount = 26;
+  const toUpperCaseDif = -32;
+  const isToUpper = Math.floor(Math.random() * 2);
+  const charCode = Math.floor((Math.random() * charsCount)) + firstCharCode + isToUpper * toUpperCaseDif;
+
+  return String.fromCharCode(charCode);
+}
+
+interface IntervalState {
+  subscriber: Subscriber<string>;
+  counter: string;
+  period: number;
+}
+
+export class MiracleSubject<T> extends Subject<T> {
+  private times = 0;
+  private observersTimes: WeakMap<Subscriber<T>, number> = new WeakMap();
+
+  constructor(private skipTimes: number) {
+    super();
+  }
+
+  next(value: T): void {
+    if (this.closed) {
+      throw new ObjectUnsubscribedError();
     }
-  );
+    if (this.skipTimes > this.times++) return;
+    if (!this.isStopped) {
+      const { observers } = this;
+      observers.forEach(o => o.next(value));
+    }
+  }
+
+  // subscribe(observer?: PartialObserver<T>) {
+  //   super.subscribe(observer);
+  // };
+  // subscribe(next?: (value: T) => void, error?: (error: any) => void, complete?: () => void) {
+  //   super.subscribe(next, error, complete);
+  // }
+
+  subscribe(): void {
+
+  }
 }
 
 // http://www.introtorx.com/Content/v1.0.10621.0/15_SchedulingAndThreading.html
 // https://github.com/ReactiveX/rxjs/blob/master/src/internal/observable/interval.ts
+// https://www.pertiller.tech/blog/on-the-subject-of-multiple-subscriptions-with-rxjs
